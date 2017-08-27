5b224f1ef86095d5eef8ab36d8d7b6bc38734534
jiro <jiroron666@gmail.com>
2017-08-27 11:14:52 +0900

    random変更

--- res/js/random.js
+++ res/js/random.js
@@ -2,9 +2,12 @@ function setRandom() {
   clearCheck();
   var inputs = document.getElementsByTagName("input");
   for (var i=0; i<inputs.length; i++) {
-    var round = Math.floor(Math.random() * 10);
-    if (round == 0) {
-      inputs[i].checked = true;
+    var input = inputs[i];
+    if (input.type === "checkbox") {
+      var round = Math.floor(Math.random() * 10);
+      if (round == 0) {
+        input.checked = true;
+      }
     }
   }
   updateRecords();
